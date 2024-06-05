cb7397545487eacc03b8ddceddaeb3285ccc775d
Sergio Rodríguez <sergiorodriguezcosta10@gmail.com>
2024-06-05 12:49:36 +0200

    compilar app en pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,20 @@
-import { fileURLToPath, URL } from 'node:url'
-
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import { resolve } from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
   ],
+  build: {
+    outDir: 'docs', // Esto es importante para GitHub Pages, ya que servirá el contenido desde la carpeta 'docs'
+    assetsDir: '.', // La ruta de acceso a los activos (por ejemplo, imágenes, archivos CSS) será relativa a la raíz del sitio
+  },
+  publicDir: './', // Esta es la opción equivalente a 'base' para especificar la ruta base de tu aplicación
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
-})
+      '@': resolve(__dirname, './src'), // Ruta de alias para resolver las importaciones de módulos
+    },
+  },
+});
