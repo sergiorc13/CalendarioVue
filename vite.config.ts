d72dcdba960703c702de1f6efee73261e5c0dab5
Sergio Rodríguez <sergiorodriguezcosta10@gmail.com>
2024-06-05 12:57:29 +0200

    chore: Update tareas.json and vite.config.ts files

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,16 @@
-import { defineConfig } from 'vite';
-import vue from '@vitejs/plugin-vue';
-import { resolve } from 'path';
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
   ],
-  build: {
-    outDir: 'docs', // Esto es importante para GitHub Pages, ya que servirá el contenido desde la carpeta 'docs'
-    assetsDir: '.', // La ruta de acceso a los activos (por ejemplo, imágenes, archivos CSS) será relativa a la raíz del sitio
-  },
-  publicDir: './', // Esta es la opción equivalente a 'base' para especificar la ruta base de tu aplicación
   resolve: {
     alias: {
-      '@': resolve(__dirname, './src'), // Ruta de alias para resolver las importaciones de módulos
-    },
-  },
-});
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  }
+})
